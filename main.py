dc8ddb546b71c9d7af59a58ace8ec61530c58d50
Mark William <markdwilliams82@gmail.com>
2018-10-15 15:53:50 -0500

    finished

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
-# Note: the connection string after :// contains the following info:
-# user:password@server:portNumber/databaseName
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode!@localhost:8889/build-a-blog'
-app.config['SQLALCHEMY_ECHO'] = True # useful for debugging issues you may have when your application isn't talking to your database the way you expect it to
+app.config['SQLALCHEMY_ECHO'] = True 
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
@@ -22,14 +18,52 @@ class Blog(db.Model):
         self.body = body
 
 
+@app.route('/')
+def index():
+    return redirect('/blog')
+
 @app.route("/blog", methods=['GET', 'POST'])
 def blog():
-    return render_template('/blog.html')
+
+    blogs = Blog.query.all()
+    blog_id = request.args.get('id')
+    if blog_id != None:
+        blog = Blog.query.get(blog_id)
+        return render_template('blog_entry.html', blog=blog)
+    
+    return render_template('blog.html', blogs=blogs)
+
+    
 
 
 @app.route("/newpost", methods=['GET', 'POST'])
 def newpost():
-    return render_template('/newpost.html')    
+
+    blog_title_error = ""
+    blog_body_error = ""
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        
+        if body == "":
+            blog_body_error = 'Please fill in the body'
+        if title == "":
+            blog_title_error = 'Please fill in the title'
+        if not blog_body_error and not blog_title_error:
+            new_entry = Blog(title, body)
+            db.session.add(new_entry)
+            db.session.commit()
+            id = new_entry.id
+            return redirect('/blog?id={}'.format(id))
+        else:
+            return render_template('newpost.html', title=title, body=body, 
+            blog_title_error=blog_title_error, blog_body_error=blog_body_error)
+
+    else:
+        return render_template('newpost.html')
+
+
 
 
 
